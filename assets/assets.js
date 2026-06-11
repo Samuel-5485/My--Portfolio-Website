73626e1c1d8427e3b732165304913ca0bc41efa7
Sami Diriba <samediriba54@gmail.com>
2026-06-12 01:19:30 +0300

    Working on About section

--- assets/assets.js
+++ assets/assets.js
@@ -16,6 +16,7 @@ import logo_dark from './logo_dark.png';
 import mail_icon from './mail_icon.png';
 import mail_icon_dark from './mail_icon_dark.png';
 import profile_img from './sami1.png';
+import about_image from './about-image-1.png';
 import download_icon from './download-icon.png';
 import hand_icon from './hand-icon.png';
 import header_bg_color from './header-bg-color.png';
@@ -55,6 +56,7 @@ export const assets = {
     mail_icon,
     mail_icon_dark,
     profile_img,
+    about_image,
     download_icon,
     hand_icon,
     header_bg_color,
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,15 +4,13 @@ import {assets} from "@/assets/assets";
 
 const Header = ()=>{
     return(
-        <div>
+        <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-1'>
             <Image src={assets.profile_img} alt="" className='rounded-full w-32'/>
-            <h3>Hi! I'm Sami Diriba <Image src={assets.hand_icon} alt="" className='w-6'/></h3>
+            <h3 className='flex items-center gap-2'>Hi! I'm Sami Diriba <Image src={assets.hand_icon} alt="" className='w-5'/></h3>
             <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer.</h1>
             <p className='max-w-2xl mx-auto font-Ova'>
                 I am a passionate and skilled full stack developer with expertise in both frontend and backedend developer technologies.
-                 I have a strong foundation in HTML, CSS, JavaScript, and various frontend frameworks such as React and Vue.js. On the backend, 
-                 I am proficient in Node.js, Express, and databases like MongoDB and MySQL.
-                 I am dedicated to creating seamless and efficient web applications that provide exceptional user experiences. With a keen eye for detail and a commitment to delivering high-quality code, I strive to bring innovative solutions to every project I work on.
+                 I have a strong foundation in HTML, CSS, JavaScript, and various frontend frameworks. 
             </p>
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
                 <a href="#contact" className='px-10 py-3 border-white rounded-full bg-black 

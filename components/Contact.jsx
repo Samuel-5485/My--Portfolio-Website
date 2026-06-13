06b02a776560c4d5bf439d45392d53bf54663c5b
Sami Diriba <samediriba54@gmail.com>
2026-06-13 16:16:01 +0300

    am adding some features like creating textarea, input and adding enter name and email with required

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
-            <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+    <div className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
+            <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
             
             <p className="text-center mx-w-2xl mx-auto mt-5 mb-12 font-Ova">
-               Welcome to my web development portfolio! Explore a collection of projects 
-               showcasing my expertise in front-end development.
+               I'd love to hear from you! If you have any questions, comments, or feedback
+               please use the form below.
             </p>
+            <form className='max-w-2xl mx-auto'>
+                <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+                    <input type='text' placeholder='Enter your name' required
+                    className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
+                    rounded-md bg-white'/>
+                    <input type='email' placeholder='Enter your email' required
+                    className='flex-1 p-3 outline-none border-[0.5px] border-gray-400
+                    rounded-md bg-white'/>
+                </div>
+                <textarea rows='6'placeholder='Enter your message'required></textarea>
+                <button type='submit'>Submit now</button>
+            </form>
     </div>
   )
 }
 
-export default Contact
+export default Contact;

99fcffce4c4d67e8d1f14dd6fbf6222fd3cbe423
Sami Diriba <samediriba54@gmail.com>
2026-06-13 17:51:46 +0300

    working on theme whether u like white or black theme

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const About = ()=>{
     return(
-        <div className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+        <div id="about" className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
             <h2 className='text-center text-5xl font-Ovo'>About Me</h2>
             <div className='flex w-full flex-col lg:flex-row items-center gap-20 mt-20'>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,6 +13,16 @@ const Footer = () => {
         samediriba54@gmail.com
     </div>
     </div>
+    <div className='text-center sm:flex items-center justify-between border-t border-gray-400
+    mx-[10%] mt-12 py-6'>
+        <p>© 2026 Sami Diriba. All rights reserved.</p>
+        <ul className='flex items-center gap-10 justify-center mt-5 sm:mt-0'>
+            <li><a target='_blank' href='www.linkedin.com/in/samuel-diriba177866'></a>LinkedIn</li>
+            <li><a target='_blank' href='https://t.me/samitechacademy'></a>Telegram</li>
+            <li><a target='_blank' href='https://github.com/Samuel-5485/'></a>GitHub</li>
+            
+        </ul>
+    </div>
     </div>
   )
 }
